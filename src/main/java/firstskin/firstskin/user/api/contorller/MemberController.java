0abea0630b808cc6db2c208427770cb682e39293
ch1-9898 <jso1998@naver.com>
2024-04-24 17:16:37 +0900

    사용자 기능

---
+++ src/main/java/firstskin/firstskin/user/api/contorller/MemberController.java
@@ -0,0 +1,88 @@
+package firstskin.firstskin.user.api.contorller;
+
+import firstskin.firstskin.member.domain.Member;
+import firstskin.firstskin.model.KakaoProfile;
+import firstskin.firstskin.model.OauthToken;
+import firstskin.firstskin.user.api.dto.MemberDto;
+import firstskin.firstskin.user.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import static firstskin.firstskin.member.domain.Role.ROLE_USER;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    final private MemberService memberService;
+
+
+    @GetMapping("/oauth2/kakao/callback")
+    public ResponseEntity<String> login(@RequestParam String code, HttpServletRequest httpServletRequest){
+
+        OauthToken oauthToken = memberService.requestToken(code);
+        KakaoProfile kakaoProfile = memberService.requestKakaoProfile(oauthToken);
+
+        Member existingMember = memberService.findMemberById(kakaoProfile.getId().toString());
+
+        if (existingMember != null) {
+            memberService.sessionSave(httpServletRequest, existingMember, oauthToken);
+            return ResponseEntity.status(HttpStatus.OK).body("Logged-in");
+        } else {
+            Member newMember = new Member(
+                    ROLE_USER,
+                    kakaoProfile.getKakao_account().getProfile().getProfile_image_url(),
+                    kakaoProfile.getId().toString(),
+                    kakaoProfile.getKakao_account().getProfile().getNickname());
+
+            memberService.addMember(newMember);
+            memberService.sessionSave(httpServletRequest, newMember, oauthToken);
+
+            return ResponseEntity.status(HttpStatus.OK).body("new-member");
+        }
+    }
+    @GetMapping("/logout-kakao")
+    public ResponseEntity<String> logoutKakao(HttpServletRequest request) {
+        String accessToken = (String) request.getSession().getAttribute("access_token");
+
+        memberService.logoutRequest(accessToken);
+
+        HttpSession session = request.getSession(false);
+        if(session != null) {
+            session.invalidate();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body("Logged-out-successfully");
+
+    }
+    @GetMapping("/members")
+    public ResponseEntity<List<MemberDto>> getAllMembers() {
+        List<MemberDto> members = memberService.getAllMembers();
+
+        return new ResponseEntity<>(members, HttpStatus.OK);
+    }
+
+    @GetMapping("/members/{memberId}")
+    public ResponseEntity<Optional<MemberDto>> getMember(@PathVariable Long memberId){
+        Optional<MemberDto> member = memberService.getMemberById(memberId);
+        return ResponseEntity.status(HttpStatus.OK).body(member);
+    }
+
+    @PutMapping("/members/{memberId}")
+    public ResponseEntity<String> updateProfile(@PathVariable Long memberId, @RequestBody MemberDto memberDto) {
+            memberService.updateProfile(memberId, memberDto);
+            return ResponseEntity.ok("Profile updated successfully");
+
+    }
+
+
+}
+
+
---
+++ src/main/java/firstskin/firstskin/user/service/MemberService.java
@@ -0,0 +1,163 @@
+package firstskin.firstskin.user.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import firstskin.firstskin.category.domain.Category;
+import firstskin.firstskin.member.domain.Member;
+import firstskin.firstskin.member.domain.Role;
+import firstskin.firstskin.member.repository.MemberRepository;
+import firstskin.firstskin.model.KakaoProfile;
+import firstskin.firstskin.model.OauthToken;
+import firstskin.firstskin.user.api.dto.MemberDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static firstskin.firstskin.member.domain.Role.ROLE_USER;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository, ObjectMapper objectMapper) {
+        this.memberRepository = memberRepository;
+        this.objectMapper = objectMapper;
+    }
+
+
+    public Member findMemberById(String userId) {
+        Optional<Member> findMember = Optional.ofNullable(memberRepository.findByUserId(userId));
+
+        return findMember.orElse(null);
+    }
+
+    public void addMember(Member member) {
+        memberRepository.save(member);
+    }
+
+    public List<MemberDto> getAllMembers() {
+        List<Member> members = memberRepository.findAll();
+        return members.stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+    }
+    public Optional<MemberDto> getMemberById(Long memberId) {
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        if (optionalMember.isPresent()) {
+            Member member = optionalMember.get();
+            return Optional.of(mapToDto(member));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public void updateProfile(Long memberId, MemberDto memberDto) {
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        if (optionalMember.isPresent()) {
+            Member member = optionalMember.get();
+            memberDto.updateMember(member.getNickname(), member.getProfileUrl());
+            memberRepository.save(member);
+        } else {
+            throw new RuntimeException("Member not found with id: " + memberId);
+        }
+    }
+
+
+
+    private MemberDto mapToDto(Member member) {
+        return new MemberDto(
+                member.getNickname(),
+                member.getProfileUrl(),
+                member.getRole()
+        );
+    }
+
+
+
+
+
+
+    public OauthToken requestToken(String code) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type","authorization_code");
+        params.add("client_id","c33ec31ce21c44a27c43a6165664cb5a");
+        params.add("redirect_uri","http://localhost:8080/oauth2/kakao/callback");
+        params.add("code",code);
+
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        try {
+            return objectMapper.readValue(response.getBody(), OauthToken.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public KakaoProfile requestKakaoProfile(OauthToken oauthToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization","Bearer "+oauthToken.getAccess_token());
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                kakaoProfileRequest,
+                String.class
+        );
+
+        try {
+            return objectMapper.readValue(response.getBody(), KakaoProfile.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void logoutRequest(String token) {
+        String kakaoAccessToken = token;
+
+        String logoutUrl = "https://kapi.kakao.com/v1/user/logout";
+
+
+        restTemplate.postForObject(logoutUrl + "?access_token=" + kakaoAccessToken, null, String.class);
+    }
+
+    public void sessionSave(HttpServletRequest httpServletRequest, Member member, OauthToken oauthToken) {
+        httpServletRequest.getSession().invalidate();
+        HttpSession session = httpServletRequest.getSession(true);
+        session.setAttribute("memberId", member.getMemberId());
+        session.setAttribute("access_token", oauthToken.getAccess_token());
+        session.setMaxInactiveInterval(3600);
+    }
+
+
+    }
+
