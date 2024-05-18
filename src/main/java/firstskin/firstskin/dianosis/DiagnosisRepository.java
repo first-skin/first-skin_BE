8007e8b1cde886d6065596ddd7ea3d9874a4084f
ch1-9898 <jso1998@naver.com>
2024-05-18 21:15:04 +0900

    진단 리포지토리

--- src/main/java/firstskin/firstskin/dianosis/DiagnosisRepository.java
+++ src/main/java/firstskin/firstskin/dianosis/DiagnosisRepository.java
@@ -2,9 +2,12 @@ package firstskin.firstskin.dianosis;
 
 import firstskin.firstskin.dianosis.domain.Diagnosis;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface DiagnosisRepository extends JpaRepository<Diagnosis, Long> {
-    Diagnosis findByCreatedDate(LocalDateTime startDate, LocalDateTime endDate);
+    Optional<Diagnosis> findByCreatedDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
