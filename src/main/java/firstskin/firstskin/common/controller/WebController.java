dbd4c691989a5d633501ecf70435e72c18596ada
WowJo <dst04157@gmail.com>
2024-06-04 01:17:17 +0900

    feat: 인덱스 페이지 추가

--- src/main/java/firstskin/firstskin/common/controller/WebController.java
+++ src/main/java/firstskin/firstskin/common/controller/WebController.java
@@ -2,6 +2,7 @@ package firstskin.firstskin.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
@@ -9,5 +10,16 @@ public class WebController {
     @GetMapping("/admin")
     public String forwardAdmin() {
         return "forward:/web/admin/index.html";  // URL을 유지하면서 내부적으로 전달
+    }
+
+    @GetMapping("/")
+    public String forwardMain() {
+        return "forward:/web/index.html";
+    }
+
+    @RequestMapping(value = "/{path:^(?!api|web|admin|swagger).*}/**")
+    public String redirect() {
+        // 모든 비정적 파일 요청을 index.html로 리디렉션
+        return "forward:/web/admin/index.html";
     }
 }
