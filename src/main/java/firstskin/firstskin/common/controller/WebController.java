f0b18a620aadffa93bb10cd77aa357af82497919
WowJo <dst04157@gmail.com>
2024-06-06 22:51:46 +0900

    fix: static 경로 변경

--- src/main/java/firstskin/firstskin/common/controller/WebController.java
+++ src/main/java/firstskin/firstskin/common/controller/WebController.java
@@ -2,24 +2,14 @@ package firstskin.firstskin.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
 
-    @GetMapping("/admin")
-    public String forwardAdmin() {
-        return "forward:/web/admin/index.html";  // URL을 유지하면서 내부적으로 전달
-    }
 
-    @GetMapping("/")
+    @GetMapping(value = {"/", "/login", "/MemberList","/category", "/restudy", "/logout"})
     public String forwardMain() {
-        return "forward:/web/index.html";
+        return "forward:/index.html";
     }
 
-    @RequestMapping(value = "/{path:^(?!api|web|admin|swagger).*}/**")
-    public String redirect() {
-        // 모든 비정적 파일 요청을 index.html로 리디렉션
-        return "forward:/web/admin/index.html";
-    }
 }
